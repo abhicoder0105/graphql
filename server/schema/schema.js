0da1e103ef63b35b30b007a3e6bddd5564127588
abhicoder0105 <sharmaabhishek9770@gmail.com>
2022-02-01 11:56:38 +0530

    defined schema and integrated schema into app

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,11 +1,30 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = require('graphql');
 
 const BookType = new GraphQLObjectType({
   name: "Book",
   fields: () =>({
     id: {type:  GraphQLString},
-    name: {type: GraphQLString} 
+    name: {type: GraphQLString},
+    genre: {type: GraphQLString},
   })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    book:{
+      type: BookType,
+      args: {id: {type: GraphQLString}},
+      resolve(parent, args) {
+        //code we need from db/ other sources
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 })
+
