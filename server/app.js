4733dd56713ecb26bf67c59118a68c114b54c684
abhicoder0105 <sharmaabhishek9770@gmail.com>
2022-02-02 13:29:07 +0530

    changes error

--- server/app.js
+++ server/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql').graphqlHTTP;
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//connection to monogdb connection
+mongoose.connect("mongodb+srv://rohit:iAC6i2zRiT3cFe9@cluster0.x1khx.mongodb.net/mongodb?retryWrites=true&w=majority")
+mongoose.connection.once('open', ()=>{
+  console.log('connected to databse');
+})
+
 
 app.use('/graphql',graphqlHTTP({
   schema,
